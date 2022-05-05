01e38e1d177dfe809aca822ea5bd51a995eda453
Robert Hogan <robhogan@gmail.com>
2022-05-05 09:07:33 +0100

    Add dist bundle

--- db/900-cmds.sql
+++ db/900-cmds.sql
@@ -24,7 +24,7 @@ create index indx_datetime on HIGH_TIDES (DATE_TIME);
 create index indx_stationid on HIGH_TIDES (STATION_ID);
 
 pragma journal_mode = delete; -- to be able to actually set page size
-pragma page_size = 4096; -- trade off of number of requests that need to be made vs overhead. 
+pragma page_size = 8192; -- trade off of number of requests that need to be made vs overhead. 
 -- insert into ftstable(ftstable) values ('optimize'); -- for every FTS table you have (if you have any)
 vacuum; -- reorganize database and apply changed page size
 
